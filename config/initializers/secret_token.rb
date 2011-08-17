98b535614fe4798e1f3ec6acb0a26a690d3ed913
gregmalcolm <greg@gregmalcolm.com>
2011-08-17 07:27:53 -0400

    Fresh rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuardJasmineCoffeescript::Application.config.secret_token = '65e22a8c72f6ab3cb4e5f8f5eceb0cfe1ccc5cf674956da4d8cc6c0a3fa916c29c19797e7e1c8945a532baf75dfaf51fb6e0d9d039dc7dc0d58a6d7283ef11b8'
